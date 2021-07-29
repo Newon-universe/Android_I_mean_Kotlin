80489e9d175a27a21a961a8be59b0a960be614d0
newon-universe <Newon_Van@icloud.com>
2021-07-29 23:37:58 +0900

    2021/07/29

--- RecyclerView_practice101/app/src/main/java/com/example/recyclerview_practice101/MainActivity.kt
+++ RecyclerView_practice101/app/src/main/java/com/example/recyclerview_practice101/MainActivity.kt
@@ -3,17 +3,26 @@ package com.example.recyclerview_practice101
 import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
+import androidx.core.widget.addTextChangedListener
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerview_practice101.databinding.ActivityMainBinding
 import com.example.recyclerview_practice101.databinding.AlertdialogEdittextBinding
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
-data class Member(val name:String, var introduce:String, var checkStatus: Boolean = false)
+
+// **sponser boolean array**
+data class Member(var title:String, var content:String = "", var time: String = "", var checkStatus: Boolean = false)
 
 class MainActivity : AppCompatActivity() {
 
-    private var adapter: MyAdapter? = null
+    private lateinit var adapter: MyAdapter
     private val data:MutableList<Member> = mutableListOf()
+
     var i = 4
 
     init{
@@ -27,8 +36,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private lateinit var binding: ActivityMainBinding
 
+    // 시간
+    val current = LocalDateTime.now()
+    val formatter = DateTimeFormatter.ofPattern("HH:mm")
+    val formatted: String = current.format(formatter)
+
+    // 정렬 기준
+    var descendingSort = false
+
+
+    private lateinit var binding: ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,25 +54,65 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
         initialize()
         adapter = MyAdapter()
-        adapter!!.listData = data
+        adapter.listData = data
         binding.recyclerView.adapter = adapter
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
 
         binding.fab.setOnClickListener {
-            val string = "Member$i"
-            data.add(Member(string, "안뇽 ^-^ $i"), )
-            i++
-            adapter?.notifyDataSetChanged()
+            createMember(this.data)
+//            val string = "Member$i"
+//            data.add(Member(string, "안뇽 ^-^ $i"),)
+//            i++
+            adapter.notifyDataSetChanged()
         }
+
+        binding.btnSort.setOnClickListener {
+            if (descendingSort == false) {
+                data.sortByDescending(Member::time)
+                descendingSort = true
+            } else {
+                data.sortBy(Member::time)
+                descendingSort = false
+            }
+
+            adapter.notifyDataSetChanged()
+        }
+
+        val itemTouchCallback = object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN, ItemTouchHelper.LEFT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                val fromPos = viewHolder.adapterPosition
+                val toPos = target.adapterPosition
+                adapter.swapData(fromPos, toPos)
+                // 움직이는 포지션은 adapterPosition
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                adapter.removeData(viewHolder.layoutPosition)
+                // 고정된 포지션은 layoutPosition
+            }
+
+        }
+        ItemTouchHelper(itemTouchCallback).attachToRecyclerView(binding.recyclerView)
+
+        // **text watcher**  or filter ?
     }
 
     private fun initialize(){
         with(data){
-            add(Member("Member1", "안뇽 ^-^ 1"))
-            add(Member("Member2", "안뇽 ^-^ 2"))
-            add(Member("Member3", "안뇽 ^-^ 3"))
+            add(Member("밥먹기", "팔도비빔면에 삼겹살", formatted))
+            add(Member("청소하기", "선풍기 닦기", formatted))
+            add(Member("고양이", "나만 고양이 없어 |나만 고양이 없어 |나만 고양이 없어 |나만 고양이 없어 |나만 고양이 없어 |", formatted))
         }
     }
 
@@ -65,10 +123,35 @@ class MainActivity : AppCompatActivity() {
 //        binding.recyclerView.layoutManager = LinearLayoutManager(this)
 //    }
 
-    fun deleteMember(member:Member){
-        data.remove(member)
-        adapter?.notifyDataSetChanged()
+//    fun deleteMember(member:Member){
+//        data.remove(member)
+//        adapter?.notifyDataSetChanged()
+//    }
+
+    private fun createMember(member: MutableList<Member>){
+
+        val builder = AlertDialog.Builder(this)
+        val builderItem = AlertdialogEdittextBinding.inflate(layoutInflater)
+        val editText = builderItem.editText
+
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("HH:mm")
+        val formatted: String = current.format(formatter)
+
+        with(builder){
+            setTitle("오늘의 할 일은 무엇인가요?")
+            setMessage("제목을 입력하세요")
+            setView(builderItem.root)
+            setPositiveButton("OK"){_:DialogInterface,_:Int ->
+                if(editText.text.toString() != null){
+                    data.add(Member(editText.text.toString(), "", formatted) )
+                    adapter.notifyDataSetChanged()
+                }
+            }
+            show()
+        }
     }
+
 
     fun editMember(position: Int, member:Member){
 
@@ -77,15 +160,13 @@ class MainActivity : AppCompatActivity() {
         val editText = builderItem.editText
 
         with(builder){
-            setTitle("Input Introduce")
-            setMessage("소개글을 입력하세요")
+            setTitle("오늘의 할 일은 무엇인가요?")
+            setMessage("세부 사항을 입력하세요")
             setView(builderItem.root)
             setPositiveButton("OK"){_:DialogInterface,_:Int ->
-                if(editText.text.toString() != null){
-                    member.introduce = editText.text.toString()
+                    member.content = editText.text.toString()
                     data[position] = member
-                    adapter!!.notifyDataSetChanged()
-                }
+                    adapter.notifyDataSetChanged()
             }
             show()
         }
--- RecyclerView_practice101/app/src/main/java/com/example/recyclerview_practice101/MyAdapter.kt
+++ RecyclerView_practice101/app/src/main/java/com/example/recyclerview_practice101/MyAdapter.kt
@@ -2,20 +2,21 @@ package com.example.recyclerview_practice101
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.CompoundButton
-import android.widget.Switch
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerview_practice101.databinding.ActivityMainBinding
 import com.example.recyclerview_practice101.databinding.RecycleItemBinding
+import com.google.android.material.snackbar.Snackbar
+import java.util.*
 
 class MyAdapter: RecyclerView.Adapter<Holder>() {
 
     var listData = mutableListOf<Member>()
 
 
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         var binding = RecycleItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return Holder(binding)
-
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
@@ -27,6 +28,20 @@ class MyAdapter: RecyclerView.Adapter<Holder>() {
         return listData.size
     }
 
+    // 뷰홀더 포지션을 받아 그 위치의 데이터를 삭제하고 notifyItemRemoved 로 어댑터에 갱신 명
+    fun removeData(position: Int) {
+        listData.removeAt(position)
+        notifyItemRemoved(position)
+    }
+
+    // 두 개의 뷰홀더 포지션을 받아 Collections.swap 으로 첫번째 위치와 두번째 위의 데이터 교환
+    fun swapData(fromPos: Int, toPos: Int){
+        Collections.swap(listData, fromPos, toPos)
+        notifyItemMoved(fromPos, toPos)
+    }
+
+
+
 }
 
 class Holder(val binding: RecycleItemBinding): RecyclerView.ViewHolder(binding.root) {
@@ -36,10 +51,6 @@ class Holder(val binding: RecycleItemBinding): RecyclerView.ViewHolder(binding.r
     var mPosition: Int? = null
 
     init {
-        binding.btnDelete.setOnClickListener {
-            mainActivity?.deleteMember(mMember!!)
-        }
-
         binding.btnEdit.setOnClickListener {
             mainActivity?.editMember(mPosition!!, mMember!!)
         }
@@ -47,13 +58,25 @@ class Holder(val binding: RecycleItemBinding): RecyclerView.ViewHolder(binding.r
 
     fun setData(member: Member, position: Int){
         with(binding){
-            name.text = member.name
-            introduce.text = member.introduce
+            title.text = member.title
+            content.text = member.content
             check.isChecked = member.checkStatus
+            time.text = member.time
 
             check.setOnClickListener {
                 mMember!!.checkStatus = check.isChecked
             }
+
+            recycleViewItemLayout.setOnClickListener {
+                check.isChecked = !check.isChecked
+                mMember!!.checkStatus = check.isChecked
+                Snackbar.make(it, "오늘의 ${layoutPosition+1}번째 목표가 선택되었습니다.", Snackbar.LENGTH_SHORT).show()
+            }
+
+            binding.profile.setOnClickListener(){
+                profile.playAnimation()
+            }
+
         }
         this.mMember = member
         this.mPosition = position
