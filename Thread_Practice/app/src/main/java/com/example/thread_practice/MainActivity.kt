f9f779f857efeaaaa0d251c7e7860b1bc2e36325
newon-universe <Newon_Van@icloud.com>
2021-08-01 23:14:24 +0900

    2021/08/01

--- Thread_Practice/app/src/main/java/com/example/thread_practice/MainActivity.kt
+++ Thread_Practice/app/src/main/java/com/example/thread_practice/MainActivity.kt
@@ -1,21 +1,31 @@
 package com.example.thread_practice
 
 import android.animation.ObjectAnimator
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.widget.ImageView
 import com.example.thread_practice.databinding.ActivityMainBinding
+import io.github.controlwear.virtual.joystick.android.JoystickView
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
 
 import kotlin.random.Random as Random
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = "조이스틱"
 
     var handler: Handler = Handler()
     var runnable: Runnable = Runnable {  }
 
     private lateinit var binding: ActivityMainBinding
+
+    private var x: Float = 0.0F
+    private var y: Float = 0.0F
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +37,19 @@ class MainActivity : AppCompatActivity() {
 
         imagemove(binding.imageView1, -400f, 400f, timer)
 
+
+
+        val joystick: JoystickView = binding.joystick
+        joystick.setOnMoveListener({ angle: Int, strength: Int ->
+
+            x += (strength * 0.3 * cos((1 - angle.toFloat() / 360) * 2 * PI )).toFloat()
+            y += (strength * 0.3 * sin((1 - angle.toFloat() / 360) * 2 * PI )).toFloat()
+
+            Log.d(TAG, "$x, $y")
+            imagemove(binding.player, x, y, 25)
+
+
+        }, 25)
 
     }
 
@@ -43,12 +66,11 @@ class MainActivity : AppCompatActivity() {
                     duration = duration1
                     start()
                 }
-                handler.postDelayed(runnable, duration1)
+//                handler.postDelayed(runnable, duration1)
             }
         }
         handler.post(runnable)
     }
-
 
 
     fun time(from: Int, to: Int) : Int {
